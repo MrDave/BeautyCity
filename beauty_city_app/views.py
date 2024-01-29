07e8a6d8be807bbf9d6558fd5836d3fd93b8c6a3
David Schnoll <davidschnoll@gmail.com>
2024-01-29 15:00:11 +0200

    Fix views.py merges

--- beauty_city_app/views.py
+++ beauty_city_app/views.py
@@ -1,15 +1,14 @@
-from django.shortcuts import get_object_or_404, render
-from django.db.models import Sum
-from django.contrib.auth.decorators import login_required
-from beauty_city_app.models import Appointment
-from django.utils import timezone
-from django.db.models import Count
+from datetime import date, datetime
 
+from django.contrib.auth.decorators import login_required
+from django.db.models import Count, Sum
+from django.shortcuts import get_object_or_404, render
+from django.utils import timezone
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from beauty_city_app.models import Appointment, Service, Shop, Specialist, Client
+from beauty_city_app.models import Appointment, Service, Shop, Specialist, Client, TimeSlot
 
 
 def index(request):
@@ -44,42 +43,50 @@ def service(request):
     shops = [(pk, f'{name} {address}') for pk, name, address
              in Shop.objects.values_list('pk', 'name', 'address')]
     shops.insert(0, ('0', 'Любой салон'))
+    service_types = list(map(list, Service.SERVICE_TYPES))
     # Заглушка для выбора таймслотов
     context = {
         'shops': shops,
-        'timeslots': {
-            'morning': [
-                '10:00', '10:30',
-            ],
-            'day': [
-                '12:00', '12:30', '15:00', '16:30',
-            ],
-            'evening': [
-                '17:00', '18:30', '19:00',
-            ]
-        }
+        'service_types': service_types,
+        'timeslots': {'morning': [], 'day': [], 'evening': [], },
     }
     return render(request, 'service.html', context)
 
 
 def service_final(request):
-    shops_queryset = Shop.objects.values('pk', 'name', 'address', )
-    shop = get_object_or_404(shops_queryset, pk=request.GET.get('shop'))
+    shop_id = request.GET.get('shop', '0')
+    service_id = request.GET.get('service', '0')
+    specialist_id = request.GET.get('specialist', '0')
+    selected_date = request.GET.get('date', '')
+    selected_time = request.GET.get('time', '')
+
+    if shop_id == '0':
+        shop = {'pk': '0', 'name': 'Любой салон', 'address': ''}
+    else:
+        shops_queryset = Shop.objects.values('pk', 'name', 'address', )
+        shop = get_object_or_404(shops_queryset, pk=shop_id)
     services_queryset = Service.objects.values('pk', 'name', 'price', )
-    service = get_object_or_404(services_queryset,
-                                pk=request.GET.get('service'))
+    service = get_object_or_404(services_queryset, pk=service_id)
+    date_repr = '{:%d %b %Y}'.format(datetime
+                                     .strptime(selected_date, '%Y-%m-%d'))
+    if specialist_id == '0':
+        specialist = {'pk': '0', 'name': 'Любой специалист',
+                      'photo': '/static/img/masters/avatar/all.svg', }
+    else:
+        specialist_queryset = Specialist.objects.values('pk', 'name',
+                                                        'profile_picture', )
+        specialist = get_object_or_404(specialist_queryset, pk=specialist_id)
     # Пример значений для заполнения шаблона
     context = {
         'id': '?????',
-        'shop': shop,
+        'shop':  shop,
         'service': service,
         'timeslot': {
-            'time': '16:30', 'date': '18 ноября',
+            'time': selected_time,
+            'date_repr': date_repr,
+            'date': selected_date,
         },
-        'specialist': {
-            'name': 'Елена Грибнова',
-            'photo': '/static/img/masters/avatar/vizajist1.svg'
-        }
+        'specialist': specialist,
     }
     print(context)
     return render(request, 'serviceFinally.html', context)
@@ -146,32 +153,61 @@ def manager(request):
     }
 
     return render(request, 'admin.html', context)
-    
-    
-@api_view(['GET', 'POST,'])
-def get_select_tag_payload(request):
-    print(f'{request.GET=}')
 
-    shop = request.GET.get('shop')
-    if not shop:
+@api_view(['GET', 'POST,'])
+def get_services(request):
+    service_type = request.GET.get('service_type', '')
+    if service_type == '':
         return Response(status=status.HTTP_400_BAD_REQUEST)
-    service_type = request.GET.get('service_type')
-    if not service_type:
-        return Response({'service_types':
-                         list(map(list, Service.SERVICE_TYPES))})
-    service = request.GET.get('service')
-    if not service:
-        return Response({'services':
-                         list(map(list, Service.objects
-                                  .filter(service_type=service_type)
-                                  .values_list('pk', 'name')))})
-    specialist = request.GET.get('specialist')
-    if not specialist:
-        return Response({
-            'specialists': [{
-                'pk': '4', 'name': 'Мария Максимова',
-            }, {
-                'pk': '5', 'name': 'Анастасия Сергеева',
-            },]
-        })
-    return Response(status=status.HTTP_400_BAD_REQUEST)
+    return Response({'services': list(map(list, Service.objects
+                                 .filter(service_type=service_type)
+                                 .values_list('pk', 'name')))})
+
+
+@api_view(['GET', 'POST,'])
+def get_specialists(request):
+    shop = request.GET.get('shop', '')
+    service = request.GET.get('service', '')
+    free_slots = (TimeSlot.objects.filter(appointment__isnull=True)
+                  .prefetch_related("specialist"))
+    service_specialists = Specialist.objects.filter(services=service)
+    if shop != '0':
+        free_slots = free_slots.filter(shop__id=shop)
+    specialists = (free_slots.filter(specialist__in=service_specialists)
+                             .distinct()
+                             .values_list('specialist', 'specialist__name'))
+    specialists_serialized = list(map(list, specialists))
+    specialists_serialized.insert(0, [0, 'Любой специалист'])
+    return Response({'specialists': specialists_serialized})
+
+
+@api_view(['GET', 'POST,'])
+def get_free_timeslots(request):
+    shop_id = int(request.GET.get('shop', '0'))
+    service_id = int(request.GET.get('service', '0'))
+    specialist_id = int(request.GET.get('specialist', '0'))
+    selected_date = request.GET.get('date', f'{date.today()}')
+    free_slots = (TimeSlot.objects.filter(appointment__isnull=True)
+                  .prefetch_related("specialist"))
+    service_specialists = Specialist.objects.filter(services=service_id)
+    if shop_id:
+        free_slots = free_slots.filter(shop__id=shop_id)
+    if not specialist_id:
+        free_slots = free_slots.filter(specialist__in=service_specialists)
+    else:
+        free_slots = free_slots.filter(specialist__id=specialist_id)
+    morning_slots = (free_slots.filter(date=selected_date)
+                     .filter(time__lt="12:00").values_list('time', flat=True)
+                     .distinct())
+    afternoon_slots = (free_slots.filter(date=selected_date)
+                       .filter(time__range=("12:00", "16:59"))
+                       .values_list('time', flat=True).distinct())
+    evening_slots = (free_slots.filter(date=selected_date)
+                     .filter(time__gte="17:00").values_list('time', flat=True)
+                     .distinct())
+    context = {
+        "morning": morning_slots,
+        "afternoon": afternoon_slots,
+        "evening": evening_slots,
+    }
+    return Response(context)
