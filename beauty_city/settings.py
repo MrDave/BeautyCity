b615a7a98d1b6bcd38f245190dc68f5fee533cbb
AlexWoIf <alexey.barannikov@gmail.com>
2024-01-29 17:21:58 +0300

    add csrf trusted origins

--- beauty_city/settings.py
+++ beauty_city/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env.str("SECRET_KEY")
 DEBUG = env.bool("DEBUG", False)
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", [])
-
+CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS", [])
 
 # Application definition
 
